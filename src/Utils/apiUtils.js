adbc84f646bb4f272e72df0c85c03f5f8179ff6c
rafasu4 <67747529+rafasu4@users.noreply.github.com>
2023-01-13 01:00:10 +0200

    Update apiUtils.js

--- src/Utils/apiUtils.js
+++ src/Utils/apiUtils.js
@@ -2,8 +2,12 @@ export const handleSubmit = async (body) => {
   try {
     const response = await fetch("https://157.230.22.122:8888/submit", {
       method: "POST",
+      mode:'no-cors',
       headers: {
         "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Methods": "*"
       },
       body: JSON.stringify(body),
     });
